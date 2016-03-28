d52d7ff104e1733da12ce0102f2b1bcf1a8255cf
dali <dali@ufofactory.org>
2016-03-28 19:04:07 +0900

    hotfix stop using proxy in crawler

--- app/crawlers/open_graph.rb
+++ app/crawlers/open_graph.rb
@@ -24,7 +24,7 @@ class OpenGraph
   def parse_opengraph(options = {})
     begin
       agent = Mechanize.new
-      agent.set_proxy '114.203.85.68', 5000 if Rails.env.production?
+      #agent.set_proxy '114.203.85.68', 5000 if Rails.env.production?
       agent.user_agent_alias = 'Windows IE 10'
       agent.follow_meta_refresh = true
       agent.redirect_ok = :all
