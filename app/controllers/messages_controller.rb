1fd2f86609191e1711d76f1568c2c11d29704385
dali <dali@ufofactory.org>
2016-03-31 22:57:58 +0900

    backdoor of messages list

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,10 @@ class MessagesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @messages = current_user.messages.recent.page params[:page]
+    if params[:user].present? and current_user.admin?
+      @user = User.find_by(nickname: params[:user])
+    end
+    @user ||= current_user
+    @messages = @user.messages.recent.page params[:page]
   end
 end
