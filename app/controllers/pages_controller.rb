e8181a22c2d0dda242bc234c21995d3575498444
dali <dali@ufofactory.org>
2016-04-05 13:56:01 +0900

    add my parti menu in header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   def home
-    if user_signed_in?
-      redirect_to :dashboard
-    else
-      redirect_to issue_home_path(Issue::OF_ALL)
-    end
+    redirect_to issue_home_path(Issue::OF_ALL)
   end
 
   def about
