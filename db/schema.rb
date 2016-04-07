54387856646428f81ce6da8a82d1ebc764d49b67
Leesunhwa <sunhwa1106@gmail.com>
2016-04-07 11:03:31 +0900

    header

--- db/schema.rb
+++ db/schema.rb
@@ -127,9 +127,6 @@ ActiveRecord::Schema.define(version: 20160403234429) do
   add_index "messages", ["messagable_type", "messagable_id"], name: "index_messages_on_messagable_type_and_messagable_id"
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
-# Could not dump table "old_articles" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
-
   create_table "old_users", force: :cascade do |t|
     t.string   "email",      null: false
     t.datetime "created_at", null: false
