3e731fc4920ea9dfeb3dbb03954d3e902bfae559
dali <dali@ufofactory.org>
2016-04-11 09:42:03 +0900

    rescue from InvalidCrossOriginRequest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,14 +18,17 @@ class ApplicationController < ActionController::Base
       render_404
     end
     rescue_from CanCan::AccessDenied do |exception|
+      self.response_body = nil
       redirect_to root_url, :alert => exception.message
     end
-    rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    rescue_from ActionController::InvalidCrossOriginRequest, ActionController::InvalidAuthenticityToken do |exception|
+      self.response_body = nil
       redirect_to root_url, :alert => I18n.t('errors.messages.invalid_auth_token')
     end
   end
 
   def render_404
+    self.response_body = nil
     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
 
