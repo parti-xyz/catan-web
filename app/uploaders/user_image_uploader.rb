3950fee2719aa6fee0667ed5895c75ba849bb4c6
dali <dali@ufofactory.org>
2016-04-12 23:18:05 +0900

    rename user image folder

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -13,7 +13,7 @@ class UserImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/images/#{model.id / 1000}"
+    "uploads/user/#{model.id / 1000}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
