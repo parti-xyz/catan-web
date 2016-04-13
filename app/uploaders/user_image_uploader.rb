53f253bdf70292a2fc267d3de329aedb2c7d0b1a
dali <dali@ufofactory.org>
2016-04-13 13:44:54 +0900

    hotfix storage

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -3,11 +3,11 @@
 class UserImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
+  #if Rails.env.production?
+  #  storage :fog
+  #else
     storage :file
-  end
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
