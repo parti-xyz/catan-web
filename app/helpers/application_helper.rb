c3bd3c62b5f86940fff3a43cb0661a36fde19fb4
dali <dali@ufofactory.org>
2016-04-22 07:52:01 +0900

    bugfix excerpt with null body of article

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def excerpt(text)
-    truncate((strip_tags(text).html_safe), length: 200)
+    truncate((strip_tags(text).try(:html_safe)), length: 200)
   end
 
   def date_f(date)
