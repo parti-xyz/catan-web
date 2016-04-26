daaee9e6f65a5d1cffa646a9298db46e61977a35
Leesunhwa <sunhwa1106@ufofactory.org>
2016-04-26 14:20:51 +0900

    add mobile-menu

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,6 +1,29 @@
 $(function() {
-  var docu_height = $(document).height();
-  var mobile_list = $('.mobile-list');
+  var $docu_height = $(document).height();
+  var $mobile_list = $('.mobile-list');
+  var $mobile_menu_btn = $(".btn-mobile-menu");
+  var $mobile_menu_cancel = $(".mobile-list__close");
+  $mobile_list.css('height', $docu_height);
 
-  mobile_list.css('height', docu_height);
+  $mobile_menu_btn.on('click', function() {
+    if ( $mobile_list.is(':visible') ) {
+      $mobile_list.hide().animate({
+        right: '-260px'
+      }, 400 );
+      $('body').css( {'margin-left':'0px', 'margin-right':'0px'} );
+    } else {
+      $mobile_list.show().animate({
+        right: '0px'
+      }, 400 );
+      $('body').animate( { marginLeft: '-260px', marginRight: '260px' } );
+    }
+  });
+  $mobile_menu_cancel.on('click', function() {
+    $mobile_list.hide().animate({
+      right: '-260px'
+    }, 400 );
+    $('body').css( {'margin-left':'0px', 'margin-right':'0px'} );
+  });
+
 });
+
