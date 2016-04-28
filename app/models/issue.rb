49ce0e41e9f82e205c5ea76cfca10107f04b9a8e
dali <dalikim@parti.xyz>
2016-04-28 21:26:59 +0900

    bugfix latest comment count in issues list tab

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -135,7 +135,7 @@ class Issue < ActiveRecord::Base
     counts.articles_count = articles.count
     counts.latest_articles_count = articles.latest.count
     counts.comments_count = posts.sum(:comments_count)
-    counts.latest_comments_count = posts.latest.sum(:comments_count)
+    counts.latest_comments_count = comments.latest.count
     counts.opinions_count = opinions.count
     counts.latest_opinions_count = opinions.latest.count
     counts.talks_count = talks.count
