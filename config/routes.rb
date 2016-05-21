1530e70870b3265226d6cc4eb4378066db110acc
rest515 <rest515@gmail.com>
2016-05-21 11:28:02 +0900

    change root path to issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root 'pages#home'
+  root 'issues#index'
 
   get '/robots.:format' => 'pages#robots'
 
