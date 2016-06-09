fd63f5f5d03b7d7a58ca820a2adaab8b984fd31e
dali <dalikim@parti.xyz>
2016-06-09 18:03:31 +0900

    new home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,7 @@ class PagesController < ApplicationController
     slugs = %w(feminism basic-income digital-social-innovation workandlife_integration household_chemicals 20th-assembly environment climate-change sewolho)
     @recommend_issues_map = Issue.where(slug: slugs).map { |issue| {issue: issue, image_url: issue.logo.url} }
 
-    bad_issues = Issue.where(slug: 'heelo')
-    valid_posts = Post.where.not(issue: bad_issues)
-    @recommend_article_posts = valid_posts.only_articles.hottest.limit(9)
-    @recommend_talk_posts = valid_posts.only_talks.hottest.limit(3)
-    @recommend_opinion_posts = valid_posts.only_opinions.hottest.limit(10)
+    @issues = Issue.all
   end
 
   def about
