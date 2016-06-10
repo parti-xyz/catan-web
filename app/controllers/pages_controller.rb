a954edee434e60aec08060d0225ca44e6e3799bf
dali <dalikim@parti.xyz>
2016-06-10 17:36:08 +0900

    mobile slide in home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,8 @@ class PagesController < ApplicationController
     #slugs = %w(feminism basic-income digital-social-innovation workandlife_integration household_chemicals 20th-assembly environment climate-change sewolho)
     #@recommend_issues_map = Issue.where(slug: slugs).map { |issue| {issue: issue, image_url: issue.logo.url} }
     @recommend_map = [
-      {url: 'http://up.parti.xyz', image: 'home/han21.png'},
-      {url: '/c/vplatform', image: 'home/vplatform.png'},
+      {url: 'http://up.parti.xyz', image: 'home/han21.png', mobile_image: 'home/han21-mobile.png'},
+      {url: '/c/vplatform', image: 'home/vplatform.png', mobile_image: 'home/vplatform-mobile.png'},
     ]
     @issues = Issue.all.order(:title)
   end
