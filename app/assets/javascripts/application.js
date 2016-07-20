efed64cf5806abd040482a8ae887a797bc581f93
dali <dalikim@parti.xyz>
2016-07-20 10:22:48 +0900

    위키 편집창 스타일

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -795,10 +795,119 @@ $(function(){
 
   // SimpleMDE
   if ( $( ".simplemde" ).length ){
+    var $elm = $($(".simplemde")[0])
     var simplemde = new SimpleMDE({
-     element: $(".simplemde")[0],
-     spellChecker: false
-    });
+      autosave: {
+        enabled: false,
+      },
+      element: $(".simplemde")[0],
+      spellChecker: false,
+      status: false,
+      toolbar: [
+        {
+          name: "bold",
+          action: SimpleMDE.toggleBold,
+          className: "fa fa-bold",
+          title: "굵게",
+        },
+        {
+          name: "italic",
+          action: SimpleMDE.toggleItalic,
+          className: "fa fa-italic",
+          title: "기울임",
+        },
+        {
+          name: "strikethrough",
+          action: SimpleMDE.toggleStrikethrough,
+          className: "fa fa-strikethrough",
+          title: "취소선"
+        },
+        {
+          name: "heading",
+          action: SimpleMDE.toggleHeadingSmaller,
+          className: "fa fa-header",
+          title: "제목",
+        },  '|',
+        {
+          name: "quote",
+          action: SimpleMDE.toggleBlockquote,
+          className: "fa fa-quote-left",
+          title: "인용",
+        },
+        {
+          name: "unordered-list",
+          action: SimpleMDE.toggleUnorderedList,
+          className: "fa fa-list-ul",
+          title: "글머리 기호 넣기",
+        },
+        {
+          name: "ordered-list",
+          action: SimpleMDE.toggleOrderedList,
+          className: "fa fa-list-ol",
+          title: "순번 매기기",
+        },  '|',
+        {
+          name: "link",
+          action: SimpleMDE.drawLink,
+          className: "fa fa-link",
+          title: "링크 연결",
+        },
+        {
+          name: "image",
+          action: SimpleMDE.drawImage,
+          className: "fa fa-picture-o",
+          title: "이미지 넣기",
+        },
+        {
+          name: "table",
+          action: SimpleMDE.drawTable,
+          className: "fa fa-table",
+          title: "표 그리기"
+        },
+        {
+          name: "horizontal-rule",
+          action: SimpleMDE.drawHorizontalRule,
+          className: "fa fa-minus",
+          title: "가로줄"
+        },  '|',
+        {
+          name: "preview",
+          action: SimpleMDE.togglePreview,
+          className: "fa fa-eye no-disable",
+          title: "미리보기",
+        },
+        {
+          name: "side-by-side",
+          action: SimpleMDE.toggleSideBySide,
+          className: "fa fa-columns no-disable no-mobile",
+          title: "미리 보면서 작성하기",
+        }, '|',
+        {
+          name: "guide",
+          action: "https://ko.wikipedia.org/wiki/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4",
+          className: "fa fa-question-circle",
+          title: "마크다운이란?",
+        },
+        {
+          name: "save",
+          action: function(editor) {
+            if($.is_present(editor.value())) {
+              editor.element.form.submit();
+            }
+          },
+          className: "btn btn-primary btn-sm btn-save-wiki",
+          title: "Save"
+        },
+        {
+          name: "취소",
+          action: $elm.data('parti-url'),
+          className: "link-to-wiki",
+          title: "취소",
+        },
+      ]
+    }).toggleFullScreen();
+    $('.btn-save-wiki').attr('data-content', $elm.data('parti-title'));
+    $('.link-to-wiki').attr('target', null);
   }
 });
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -137,7 +137,7 @@ module ApplicationHelper
   end
 
   def render_markdown_to_html(wiki_body)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, quote: true, highlight: true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, quote: true, highlight: true, strikethrough: true)
     @wiki_markdown_view = markdown.render(wiki_body)
   end
 
