d460284f1c12bddcea708a1c58dbdba3e96523dd
dali <dalikim@parti.xyz>
2016-07-29 07:27:25 +0900

    그룹 빠띠 만들 때 유니버셜 빠띠 만들기로 이동하던 버그를 수정

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,7 +2,7 @@ class IssuesController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :update, :destroy, :remove_logo, :remove_cover]
   before_filter :fetch_issue_by_slug, only: [:new_comments_count, :slug_home, :slug_users, :slug_articles, :slug_comments, :slug_opinions, :slug_talks, :slug_notes, :slug_wikis]
   load_and_authorize_resource
-  before_filter :verify_group, only: [:slug_home, :slug_articles, :slug_opinions, :slug_talks, :slug_notes, :slug_wikis, :edit, :new]
+  before_filter :verify_group, only: [:slug_home, :slug_articles, :slug_opinions, :slug_talks, :slug_notes, :slug_wikis, :edit]
   before_filter :prepare_issue_meta_tags, only: [:show, :slug_home, :slug_articles, :slug_opinions, :slug_talks, :slug_notes, :slug_wikis, :slug_users]
 
   def index
