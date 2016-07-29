a106e9741ee6d31538cea9b597901f6291a1f39e
dali <dalikim@parti.xyz>
2016-07-29 10:59:01 +0900

    서브도메인도 로그인을 기억하게 합니다

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -141,7 +141,7 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  # config.rememberable_options = {}
+  config.rememberable_options = { key: "v1lat_token", domain: :all }
 
   # ==> Configuration for :validatable
   # Range for password length.
