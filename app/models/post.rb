f7aed64c8880f5e9522c47e1741643f47445831a
dali <dalikim@parti.xyz>
2016-08-12 08:16:54 +0900

    내 홈 두 번째 페이지부터 댓글 없는 글이 나오지 않는 버그를 고칩니다

--- app/models/post.rb
+++ app/models/post.rb
@@ -47,9 +47,7 @@ class Post < ActiveRecord::Base
   scope :only_opinions, -> { by_postable_type(Opinion.to_s) }
   scope :only_talks, -> { by_postable_type(Talk.to_s) }
   scope :latest, -> { after(1.day.ago) }
-  scope :previous_of, ->(id) { where('posts.last_commented_at < ?', with_deleted.find(id).last_commented_at) if id.present? }
-  scope :previous_of_post, ->(post) { where('posts.last_commented_at < ?', post.last_commented_at) if post.present? }
-  scope :next_of_post, ->(post) { where('posts.last_commented_at >= ?', post.last_commented_at) if post.present? }
+  scope :previous_of_post, ->(post) { where('posts.last_touched_at < ?', post.last_touched_at) if post.present? }
   scope :in_group, ->(group) { joins(:issue).where('issue.group_slug' => (group.try(:slug) || group)) if group.present? }
 
   ## uploaders
