4ca4fce4175a79158b72306d60ed623265a374d1
dali <dalikim@parti.xyz>
2016-08-24 02:05:34 +0900

    검색 엔진에서 사용하는 사이트맵에 오류를 바로 잡습니다

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,6 +2,6 @@ SitemapGenerator::Sitemap.default_host = "http://parti.xyz"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.create do
   Issue.find_each do |issue|
-    add issue_home_path_or_url(issue), changefreq: 'daily', lastmod: issue.posts.newest.try(:updated_at) || issue.updated_at
+    add slug_issue_path(issue.slug), changefreq: 'daily', lastmod: issue.posts.newest.try(:updated_at) || issue.updated_at
   end
 end
