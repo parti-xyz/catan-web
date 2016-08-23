3b6fa670265b73cffb73811bfbf32931377dc1e5
dali <dalikim@parti.xyz>
2016-08-24 08:04:33 +0900

    버그 - css 컴파일 오류를 고칩니다

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,17 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(email.css application_xs.css application_default.css vendors.css mobile.js)
-Rails.application.config.assets.precompile << Proc.new do |path|
-  groups_path = 'groups'
-  if path =~ /\.(css)\z/
-    full_path = Rails.application.assets.resolve(path)
-    app_assets_path = Rails.root.join('app', 'assets', 'stylesheets', groups_path).to_path
-    if full_path.starts_with?(app_assets_path) and !path.starts_with?("#{groups_path}/_")
-      true
-    else
-      false
-    end
-  else
-    false
-  end
-end
+Rails.application.config.assets.precompile << %r{\Agroups/(?!_)([^\/]*)\.s?css\z}x
