6ba8b68786bcc30d8c304cc80452fe96e7bfbd5c
qus <qus1225@gmail.com>
2016-10-10 23:53:59 +0900

    베스트 선정 기준에 찬반투표의 투표수도 포함됩니다.  #372

--- app/jobs/statistics_job.rb
+++ app/jobs/statistics_job.rb
@@ -8,6 +8,10 @@ class StatisticsJob
       result[post_id] = (result[post_id] || 0) + upvotes_counts
     end
 
+    Vote.past_week.group(:post_id).count.each do |post_id, votes_counts|
+      result[post_id] = (result[post_id] || 0) + votes_counts
+    end
+
     result.each do |post_id, recommend_score|
       post = Post.find post_id
       post.update_columns(recommend_score: recommend_score, recommend_score_datestamp: Date.today.strftime('%Y%m%d'))
