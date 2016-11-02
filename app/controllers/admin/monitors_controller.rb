dc1786e2724f851f7846bdd3e6b5d6fe9f1e307e
dali <dalikim@parti.xyz>
2016-11-03 08:53:24 +0900

    통계치를 관리화면에서 봅니다

--- app/controllers/admin/monitors_controller.rb
+++ app/controllers/admin/monitors_controller.rb
@@ -1,5 +1,6 @@
 class Admin::MonitorsController < AdminController
   def index
     @issue_tags = ActsAsTaggableOn::Tag.where('taggings.taggable_type': 'Issue').joins(:taggings).select('name').distinct
+    @statistics = Statistics.recent.limit(50)
   end
 end
--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -1,2 +1,3 @@
 class Statistics < ActiveRecord::Base
+  scope :recent, -> { order(when: :desc) }
 end
