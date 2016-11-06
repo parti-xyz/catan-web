271429ef219748e1b389e6422fcffac7e604174d
dali <dalikim@parti.xyz>
2016-11-06 10:19:30 +0900

    모든 빠띠와 메이커인 빠띠를 API로 조회합니다

--- app/api/v1/parties.rb
+++ app/api/v1/parties.rb
@@ -4,10 +4,22 @@ module V1
     include V1::Defaults
 
     namespace :parties do
-      desc '내가 가입한 빠띠 목록을 반환합니다'
+      desc '내가 메이커가 아닌 가입만한 빠띠 목록을 반환합니다'
       oauth2
-      get :watched do
-        present :parties, resource_owner.watched_issues
+      get :joined_only do
+        present :parties, resource_owner.only_watched_issues
+      end
+
+      desc '내가 메이커인 빠띠 목록을 반환합니다'
+      oauth2
+      get :making do
+        present :parties, resource_owner.making_issues
+      end
+
+      desc '내가 모든 빠띠 목록을 반환합니다'
+      oauth2
+      get do
+        present :parties, Issue.all.limit(20)
       end
     end
   end
