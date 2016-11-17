28859b4a10ca478dfd275484d48243f3356cd17b
qus <qus1225@gmail.com>
2016-11-17 15:35:23 +0900

    모든 게시글이 p태그로 감싸집니다

---
+++ db/migrate/20161117061822_add_p_tag_to_not_html_body_of_talks.rb
@@ -0,0 +1,12 @@
+class AddPTagToNotHtmlBodyOfTalks < ActiveRecord::Migration
+  include ActionView::Helpers::TextHelper
+  def change
+    ActiveRecord::Base.transaction do
+      Talk.all.to_a.select do |talk|
+        if talk.body.present? and !talk.body.start_with?('<p') and !talk.body.start_with?('<h')
+          talk.update_columns(body: simple_format(talk.body))
+        end
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117044536) do
+ActiveRecord::Schema.define(version: 20161117061822) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id", limit: 4,        null: false
