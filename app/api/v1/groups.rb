6bf45b46b53a11ee7e9513956c76305392b95981
muniv <dmtgjh@naver.com>
2016-11-21 11:20:32 +0900

    API로 각 그룹에 속한 빠띠들을 조회합니다

--- app/api/v1/groups.rb
+++ app/api/v1/groups.rb
@@ -10,6 +10,16 @@ module V1
       get :joined do
         present :groups, Group.joined_by(resource_owner), base_options
       end
+
+      desc '각 그룹에 속한 빠띠목록을 반환합니다'
+      oauth2
+      params do
+        requires :slug, type: String, desc: '그룹의 slug'
+      end
+      get ':slug/parties' do
+        group = Group.find_by_slug(params[:slug])
+        present :parties, Issue.only_group_or_all_if_blank(group).recent_touched, base_options
+      end
     end
   end
 end
