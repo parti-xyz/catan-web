92cb9a72bcd9e32ad5383f259f80c4c17a28d560
dali <dalikim@parti.xyz>
2016-11-29 17:00:17 +0900

    트위터 로그인 해서 가입하는 API에 버그를 고칩니다

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -23,8 +23,13 @@ Doorkeeper.configure do
       user = User.find_by uid: external_auth.uid, provider: external_auth.provider
       if user.present?
         user
-      elsif params[:user].try(:fetch, :nickname).present?
-        User.create_by_external_auth! external_auth, params[:user].try(:fetch, :nickname), params[:user].try(:fetch, :email)
+      elsif params[:user].try(:fetch, :nickname, nil).present?
+        email = external_auth.email || params[:user].try(:fetch, :email, nil)
+        if email.present?
+          User.create_by_external_auth! external_auth, params[:user][:nickname], email
+        else
+          raise Doorkeeper::Errors::DoorkeeperError.new(:need_nickname_and_email)
+        end
       else
         if external_auth.email.present?
           raise Doorkeeper::Errors::DoorkeeperError.new(:need_nickname)
@@ -35,7 +40,7 @@ Doorkeeper.configure do
     rescue Doorkeeper::Errors::DoorkeeperError => e
       raise e
     rescue => e
-      if User.exists? nickname: params[:user].try(:fetch, :nickname)
+      if User.exists? nickname: params[:user].try(:fetch, :nickname, nil)
         raise Doorkeeper::Errors::DoorkeeperError.new(:duplicate_nickname)
       elsif external_auth.email.blank?
         raise Doorkeeper::Errors::DoorkeeperError.new(:invalid_external_auth_email)
--- lib/doorkeeper/external_auth/twitter.rb
+++ lib/doorkeeper/external_auth/twitter.rb
@@ -8,16 +8,16 @@ module Doorkeeper
       end
 
       def uid
-        twitter_api.user.id
+        twitter_api[:id_str]
       end
 
       def email
-        twitter_api.user.email
+        twitter_api[:email]
       end
 
       def image_url
         guard do
-          @image_url ||= twitter_api.user.profile_image_uri(:original)
+          @image_url ||= @init_twitter_client.user.profile_image_uri(:original)
         end
       end
 
@@ -29,12 +29,15 @@ module Doorkeeper
 
       def twitter_api
         guard do
-          @twitter_api ||= ::Twitter::REST::Client.new do |config|
+          @init_twitter_client ||= ::Twitter::REST::Client.new do |config|
             config.consumer_key = ENV['TWITTER_APP_ID']
             config.consumer_secret = ENV['TWITTER_APP_SECRET']
             config.access_token = @auth_code
             config.access_token_secret = @secret
           end
+
+          @twitter_api = ::Twitter::REST::Request.new(@init_twitter_client, :get,
+            'https://api.twitter.com/1.1/account/verify_credentials.json', include_email: true).perform
         end
       end
 
