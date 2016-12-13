bfa6058267a086e916c032fa1200c8443a776b69
qus <qus1225@gmail.com>
2016-12-13 16:03:05 +0900

    휴면중인 빠띠는 api, 빠띠찾기, 통합에디터 등에서 보이지 않습니다

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   mount Redactor2Rails::Engine => '/redactor2_rails'
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  # 시민의회 그룹 빠띠는 모두 role빠띠로 리다이렉트됩니다
   constraints(subdomain: 'citizensassembly') do
     get '/posts/:id', to: redirect(subdomain: '', path: '/posts/%{id}')
     get '/', to: redirect(subdomain: '', path: '/p/role')
