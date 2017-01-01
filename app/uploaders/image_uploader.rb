d0cad49b650036a75d39d6be5c28583776983916
dali <dalikim@parti.xyz>
2017-01-01 16:28:30 +0900

    이미지업로더의 url프로토콜을 https로 변경합니다

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -83,7 +83,7 @@ class ImageUploader < CarrierWave::Uploader::Base
       if ImageUploader::env_storage == :fog
         super_result
       else
-        super_result = "http://#{ENV["HOST"]}#{super_result}" if ENV["HOST"].present?
+        super_result = "https://#{ENV["HOST"]}#{super_result}" if ENV["HOST"].present?
         super_result
       end
     else
