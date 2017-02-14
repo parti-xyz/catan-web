e8746d36acbe9ecb54fbde80f6fc2f73305b1d64
dali <dalikim@parti.xyz>
2017-02-14 10:07:38 +0900

    사용자 갤러리에 비밀빠띠 찬반투표가 노출되고 투표가 되던 오류를 바로 잡습니다

--- app/controllers/votings_controller.rb
+++ app/controllers/votings_controller.rb
@@ -3,6 +3,10 @@ class VotingsController < ApplicationController
 
   def create
     @poll = Poll.find params[:poll_id]
+    if @poll.post.blank? or @poll.post.private_blocked?(current_user)
+      render_404 and return
+    end
+
     service = VotingPollService.new(poll: @poll, current_user: current_user)
     @voting = service.send(params[:voting][:choice].to_sym)
     respond_to do |format|
