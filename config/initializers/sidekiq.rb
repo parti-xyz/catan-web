72792c63ef76ab46933fe7c3ce87fd6764220165
dali <dalikim@parti.xyz>
2017-03-09 14:54:18 +0900

    오류수정 - 스케줄 걸린 작업이 배포해도 지워지지 않던 버그를 바로 잡습니다

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,6 +12,6 @@ else
   schedule_file = "config/schedule.yml"
 
   if File.exists?(schedule_file) && Sidekiq.server?
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
   end
 end
