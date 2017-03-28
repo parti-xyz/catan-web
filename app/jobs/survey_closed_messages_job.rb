c762ce1c81142e483a288fc1ca5b47ef48d57fc4
dali <dalikim@parti.xyz>
2017-03-28 12:14:56 +0900

    설문의 게시글이 삭제되었으면 해당 설문의 종료 알림 메일을 보내지 않습니다

--- app/jobs/survey_closed_messages_job.rb
+++ app/jobs/survey_closed_messages_job.rb
@@ -4,8 +4,10 @@ class SurveyClosedMessagesJob
   def perform
     Survey.need_to_reset_sent_closed_message_at.update_all(sent_closed_message_at: nil)
     Survey.need_to_send_closed_message.each do |survey|
-      MessageService.new(survey, action: :closed).call
-      SurveyMailer.deliver_all_later_on_closed(survey)
+      unless survey.post.blank?
+        MessageService.new(survey, action: :closed).call
+        SurveyMailer.deliver_all_later_on_closed(survey)
+      end
       survey.touch(:sent_closed_message_at)
     end
   end
