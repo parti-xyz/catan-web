07af5cadb0a0cd9bc0fddb8f5b990a3bff093be9
dali <dalikim@parti.xyz>
2017-04-18 11:13:24 +0900

    공지를 발신하면 회신메일 주소가 공지 건 사람으로 세팅됩니다

--- app/mailers/pin_mailer.rb
+++ app/mailers/pin_mailer.rb
@@ -6,7 +6,7 @@ class PinMailer < ApplicationMailer
     return if @sender.blank? or @recipient.blank? or @post.blank?
     return unless @recipient.enable_mailing?
     truncated_body = @post.specific_desc_striped_tags(20)
-    mail(to: @recipient.email,
+    mail(to: @recipient.email, reply_to: @sender.email,
       subject: "[빠띠] #{@sender.nickname}님이 게시글을 공지했습니다 : #{truncated_body}")
   end
 end
