f53fbf9de96b886879b2435996a2d7650f5ad6fd
dali <dalikim@parti.xyz>
2017-05-18 08:29:19 +0900

    이미지 업로드 할 때 간헐적으로 발생할 수 있는 오류를 막습니다

--- app/uploaders/private_file_uploader.rb
+++ app/uploaders/private_file_uploader.rb
@@ -137,7 +137,7 @@ class PrivateFileUploader < CarrierWave::Uploader::Base
   protected
 
   def image?(new_file)
-    new_file.content_type.start_with? 'image'
+    new_file.content_type.try(:start_with?, 'image')
   end
 
   def secure_token(length=16)
