ddc9b7bd135f35ff41d636d39e3f514c947c87e5
dali <dalikim@parti.xyz>
2017-06-09 13:46:06 +0900

    개별빠띠가 업데이트 되어 앱에 알려 줄 때 누가 업데이트 한 것인지도 같이 보냅니다

--- app/jobs/issue_firebase_realtime_db.rb
+++ app/jobs/issue_firebase_realtime_db.rb
@@ -1,12 +1,15 @@
   class IssueFirebaseRealtimeDb
   include Sidekiq::Worker
 
-  def perform(issue_id)
+  def perform(issue_id, user_id)
     issue = Issue.find_by(id: issue_id)
     return if issue.blank?
 
     firebase = Firebase::Client.new("https://parti-xyz.firebaseio.com", Rails.root.join('config', 'firebase-serviceAccountCredentials.json'))
-    firebase.set("#{bucket_name}/parties/#{issue.id}", { last_stroked_at: issue.last_stroked_at.try(:to_time).try(:to_i) || -1 })
+    firebase.set("#{bucket_name}/parties/#{issue.id}", {
+      last_stroked_at: (issue.last_stroked_at.try(:to_time).try(:to_i) || -1),
+      last_stroked_by: user_id
+    })
   end
 
   def bucket_name
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -293,7 +293,7 @@ class Issue < ActiveRecord::Base
 
   def strok_by!(someone)
     update_columns(last_stroked_at: DateTime.now, last_stroked_user_id: someone)
-    IssueFirebaseRealtimeDb.perform_async(self.id)
+    IssueFirebaseRealtimeDb.perform_async(self.id, someone.id)
   end
 
   def members_with_deleted
