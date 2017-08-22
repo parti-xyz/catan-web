3b126fa9e323e003a9c756ad1e6146f66e3f5a09
ganguri <ganguri@parti.xyz>
2017-08-22 14:00:38 +0900

    그룹빠띠 스타일시트를 그룹 슬러그 명의 폴더별로 묶어 관리합니다

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,6 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
@@ -10,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(email.css application_xs.css application_default.css vendors.css mobile.js)
-Rails.application.config.assets.precompile << %r{\Agroups/(?!_)([^\/]*)\.(css|sass|scss)\z}x
+Rails.application.config.assets.precompile << %r{\Agroups/(?!_)([^\/]*)/(?!_)([^\/]*)\.(css|sass|scss)\z}x
 Rails.application.config.assets.precompile += %w(lightbox/*)
