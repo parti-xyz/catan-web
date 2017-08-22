99a4eca04622e41f7654eae28aa634f00cf90162
dali <dalikim@parti.xyz>
2017-08-22 13:54:25 +0900

    활성화빠띠를 그룹별로 묶어서 봅니다

--- app/controllers/admin/active_issue_stats_controller.rb
+++ app/controllers/admin/active_issue_stats_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ActiveIssueStatsController < AdminController
   def index
-    stat_at = params[:stat_at] || Date.yesterday
-    @active_issue_stats = ActiveIssueStat.by_day(stat_at, field: :stat_at)
+    @stat_at = params[:stat_at].try(:to_date) || Date.yesterday
+    @active_issue_stats = ActiveIssueStat.by_day(@stat_at, field: :stat_at).group_by { |i| i.issue.group_slug }
   end
 end
