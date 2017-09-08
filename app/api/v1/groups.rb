bc2bf1eb0d6cf04b0a316e7745960db7578f6eb6
ganguri <ganguri@parti.xyz>
2017-09-08 11:32:58 +0900

    특정 그룹의 최신글과 핀된 게시물을 보여주는 api를 생성합니다

--- app/api/v1/groups.rb
+++ app/api/v1/groups.rb
@@ -21,6 +21,23 @@ module V1
           issue.private_blocked?(current_user)
         }, base_options
       end
+
+      desc '각 공개 그룹의 최근 게시물 limit건과 각 공개 그룹 내의 빠띠에서 핀된 공지 게시물 전체를 반환합니다'
+      params do
+        requires :slug, type: String, desc: '그룹의 slug'
+        optional :limit, type: Integer, desc: '최근 게시물 갯수', default: 10
+      end
+      get ':slug/highlight_posts' do
+        target_group = Group.find_by(slug: params[:slug])
+        error!(:not_found, 404) and return if target_group.private?
+
+        parties = target_group.issues.where(private: false)
+        pinned_posts = Post.where(issue: parties).where(pinned: true)
+        recent_posts = Post.where(issue: parties).where(pinned: false).recent.limit(params[:limit])
+
+        present :pinned, pinned_posts
+        present :recent, recent_posts
+      end
     end
   end
 end
