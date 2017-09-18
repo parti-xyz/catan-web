654c7b28c08078b85ac5477bf0314fd43da94b64
dali <dalikim@parti.xyz>
2017-09-18 17:25:18 +0900

    기술적인 사항 - 개발서버에서 로그인쿠키를 실서버와 구별합니다

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_catan_1_11_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: "_catan_1_11_session#{ Rails.env.production? ? "" : "_#{Rails.env}" }", domain: :all, tld_length: 2
