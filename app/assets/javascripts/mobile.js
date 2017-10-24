689b9f3de7361b4548b66d36bd2efc1c08c8b2f2
dali <dalikim@parti.xyz>
2017-10-24 11:01:24 +0900

    사이드 메뉴 스타일을 다듬습니다

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,27 +1,27 @@
 $(function() {
   var $docu_height = $(document).height();
   var $device_width = $(window).width();
-  var $mobile_list = $('.mobile-list');
-  var $mobile_menu_btn = $('.btn-mobile-menu');
-  var $mobile_menu_cancel = $('.mobile-list__close');
+  var $drawer_list = $('.drawer-list');
+  var $drawer_menu_btn = $('.btn-drawer-menu');
+  var $drawer_menu_cancel = $('.drawer-list__close');
   var $navbar = $('.navbar');
 
-  $mobile_menu_btn.on('click', function() {
-    if ( $mobile_list.is(':visible') ) {
-      $mobile_list.hide().animate({
+  $drawer_menu_btn.on('click', function() {
+    if ( $drawer_list.is(':visible') ) {
+      $drawer_list.hide().animate({
         left: '-260px'
       }, 400 );
       $('body').css( {'margin-left':'0px', 'margin-right':'0px'} );
       $navbar.animate( {'right': '0'} );
     } else {
-      $mobile_list.show().animate({
+      $drawer_list.show().animate({
         left: '0px'
       }, 400 );
       $('body').animate( { marginRight: '-260px', marginLeft: '260px' } );
     }
   });
-  $mobile_menu_cancel.on('click', function() {
-    $mobile_list.hide().animate({
+  $drawer_menu_cancel.on('click', function() {
+    $drawer_list.hide().animate({
       left: '-260px'
     }, 400 );
     $('body').css( {'margin-left':'0px', 'margin-right':'0px'} );
