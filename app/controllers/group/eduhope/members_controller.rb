2f327e0b81cccd82116a70eb668a6d35e1cd8869
ganguri <ganguri@parti.xyz>
2017-11-14 10:42:51 +0900

    기술적인 수정사항 -- 전교조 그룹이 아닌 경우 전교조 가입절차를 타지 않도록 합니다

--- app/controllers/group/eduhope/members_controller.rb
+++ app/controllers/group/eduhope/members_controller.rb
@@ -2,6 +2,7 @@ class Group::Eduhope::MembersController < GroupBaseController
   before_filter :authenticate_user!
 
   def admit
+    redirect_to root_path and return if current_group.slug != 'eduhope'
     @member = MemberGroupService.new(group: current_group, user: current_user, description: params[:description]).call
     if @member.persisted?
       MemberMailer.deliver_all_later_on_create(@member)
