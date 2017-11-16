09d9b0d965066f2c0347f631a81f9382be1d6347
dali <dalikim@parti.xyz>
2017-11-16 19:04:39 +0900

    토론, 위키, 자료 목록에서 비공개할 빠띠의 게시물이 섞인 경우 제대로 페이징되지 않던 버그를 바로 잡습니다

--- app/controllers/links_or_files_controller.rb
+++ app/controllers/links_or_files_controller.rb
@@ -1,6 +1,6 @@
 class LinksOrFilesController < ApplicationController
   def index
-    @posts = Post.having_link_or_file.displayable_in_current_group(current_group)
+    @posts = Post.having_link_or_file.not_private_blocked_of_group(current_group, current_user)
     how_to = params[:sort] == 'recent' ? :recent : :hottest
     @posts = @posts.send(how_to).page(params[:page]).per(3*5)
   end
--- app/controllers/polls_or_surveys_controller.rb
+++ app/controllers/polls_or_surveys_controller.rb
@@ -1,6 +1,6 @@
 class PollsOrSurveysController < ApplicationController
   def index
-    @posts = Post.having_poll.or(Post.having_survey).displayable_in_current_group(current_group)
+    @posts = Post.having_poll.or(Post.having_survey).not_private_blocked_of_group(current_group, current_user)
     how_to = params[:sort] == 'recent' ? :recent : :hottest
     @posts = @posts.send(how_to).page(params[:page]).per(3*5)
   end
