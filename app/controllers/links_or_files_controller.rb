c02a4466143d7cbb96ed50645d8f94c51b470a5d
dali <dalikim@parti.xyz>
2017-12-07 20:38:05 +0900

    그룹의 자료와 토론 글 최신순을 업데이트된 순으로 바꾸고, 기본으로 최신순이 나오게 합니다

--- app/controllers/links_or_files_controller.rb
+++ app/controllers/links_or_files_controller.rb
@@ -1,7 +1,7 @@
 class LinksOrFilesController < ApplicationController
   def index
     @posts = Post.having_link_or_file.not_private_blocked_of_group(current_group, current_user)
-    how_to = params[:sort] == 'recent' ? :recent : :hottest
+    how_to = params[:sort] == 'hottest' ? :hottest : :order_by_stroked_at
     @posts = @posts.send(how_to).page(params[:page]).per(3*5)
   end
 end
