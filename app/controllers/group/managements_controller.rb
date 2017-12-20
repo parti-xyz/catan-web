16b48fd9cd68f0a79a50f1950b8785ac3137e21c
dali <dalikim@parti.xyz>
2017-12-21 00:50:20 +0900

    그룹 관리는 오거나이저만 접근할 수 있게 합니다

--- app/controllers/group/managements_controller.rb
+++ app/controllers/group/managements_controller.rb
@@ -1,4 +1,6 @@
 class Group::ManagementsController < GroupBaseController
+  before_action :only_organizer, only: [:index]
+
   def index
     organizer_group = Group.find_by(slug: 'organizer')
     @posts_pinned = organizer_group.pinned_posts(current_user)
