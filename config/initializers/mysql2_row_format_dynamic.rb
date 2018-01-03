eb15afc3b52edcfe3c2ff4e206efead18fe08a84
dali <dalikim@parti.xyz>
2018-01-03 09:45:02 +0900

    기술적인 수정 - 테이블 생성할 때 옵션을 무조건 utf8mb4로 고정합니다

--- config/initializers/mysql2_row_format_dynamic.rb
+++ config/initializers/mysql2_row_format_dynamic.rb
@@ -4,7 +4,10 @@ unless Rails.env.test?
 
       class AbstractMysqlAdapter
         def create_table_with_innodb_row_format(table_name, options = {})
-          table_options = options.merge(:options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC')
+          if options[:options].present?
+            abort "options을 지정할 수 없습니다. 마이그레이션을 중단합니다."
+          end
+          table_options = options.merge(:options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC')
           create_table_without_innodb_row_format(table_name, table_options) do |td|
             yield td if block_given?
           end
--- db/migrate/20180102230002_create_post_searchable_indices.rb
+++ db/migrate/20180102230002_create_post_searchable_indices.rb
@@ -1,6 +1,6 @@
 class CreatePostSearchableIndices < ActiveRecord::Migration
   def change
-    create_table :post_searchable_indices, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC" do |t|
+    create_table :post_searchable_indices do |t|
       t.references :post, null: false, index: true
       t.text :ngram, limit: 65535 * 5
       t.timestamps
@@ -10,10 +10,6 @@ class CreatePostSearchableIndices < ActiveRecord::Migration
 
     reversible do |dir|
       dir.up do
-        query = 'ALTER TABLE post_searchable_indices CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci'
-        ActiveRecord::Base.connection.execute query
-        say query
-
         query = 'INSERT INTO post_searchable_indices(post_id, ngram) SELECT id, body_ngram FROM posts'
         ActiveRecord::Base.connection.execute query
         say query
