6bd405ac3123474f45d2f516d7ec14de3d61991f
dali <dalikim@parti.xyz>
2018-01-12 19:58:19 +0900

    기술적인 수정 - 스파크앱의 인터페이스를 업그레이드합니다

--- app/assets/javascripts/mobile_app.js
+++ app/assets/javascripts/mobile_app.js
@@ -2,33 +2,39 @@ if (window.webkit)
 {
     var handler = window.webkit.messageHandlers.ufop;
     ufo = {
-    'showWait': function() {
-      handler.postMessage({'method':'showWait'});
-    },
-    'hideWait': function() {
-      handler.postMessage({'method':'hideWait'});
-    },
-    'post': function(action,json) {
-      handler.postMessage({'method':'post', 'arg0':action, 'arg1':JSON.stringify(json)});
-    },
+        'showWait': function() {
+            handler.postMessage({'method':'showWait'});
+        },
+        'hideWait': function() {
+            handler.postMessage({'method':'hideWait'});
+        },
+        'setAutoWait': function(s) {
+            handler.postMessage({'method':'setAutoWait', 'arg0':(s?'1':'')});
+        },
+        'post': function(action,json) {
+            handler.postMessage({'method':'post', 'arg0':action, 'arg1':JSON.stringify(json)});
+        },
         'isApp': function() { return true; }
     }
 }
 else if (typeof ufo == "undefined")
 {
-  // 테스트 완료 후 console.log 지워주세요.
-  ufo = {
-    "showWait": function() {
-      console.log("showWait()");
-    },
-    "hideWait": function() {
-      console.log("hideWait()");
-    },
-    "post": function(action,json) {
-      console.log("UfoPost(%s,%s)", action, JSON.stringify(json));
-    },
-    'isApp': function() { return false; }
-  };
+    // 테스트 완료 후 console.log 지워주세요.
+    ufo = {
+        "showWait": function() {
+            console.log("showWait()");
+        },
+        "hideWait": function() {
+            console.log("hideWait()");
+        },
+        "setAutoWait": function(s) {
+            console.log("setAutoWait(%s)",s);
+        },
+        "post": function(action,json) {
+            console.log("UfoPost(%s,%s)", action, JSON.stringify(json));
+        },
+        'isApp': function() { return false; }
+    };
 }
 else
 {
