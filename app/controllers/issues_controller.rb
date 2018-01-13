bd12aa6c8fdf119f9b8a467fc98608fb143f68f9
dali <dalikim@parti.xyz>
2018-01-13 18:04:20 +0900

    전청넷과 유스망고를 제외하고 나머지 그룹홈을 유니온 그룹홈 형식으로 바꿉니다

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -15,14 +15,12 @@ class IssuesController < ApplicationController
       @polls_and_surveys = Post.having_poll.or(Post.having_survey).not_private_blocked_of_group(current_group, current_user)
       @polls_and_surveys = @polls_and_surveys.order_by_stroked_at.limit(7)
       @recent_posts = Post.not_in_dashboard_of_group(current_group, current_user).order_by_stroked_at.limit(4)
-      if %w(union).include? current_group.slug
-        render 'group_home_union'
-      elsif %w(greenpartyjeju eduhope slowalk).include? current_group.slug
-        render 'group_home_compact'
+      if %w(youthpolicynet).include? current_group.slug
+        render 'group_home'
       elsif %w(youthmango).include? current_group.slug
         render 'group_home_parties_first'
       else
-        render 'group_home'
+        render 'group_home_union'
       end
     end
   end
