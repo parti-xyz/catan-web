320f16dcc7723efacf9f45ec16231bbaf4752966
dali <dalikim@parti.xyz>
2018-02-09 08:41:34 +0900

    찬반투표 누가 어떻게 했는지 현황을 볼 수 있습니다

--- app/models/concerns/choosable.rb
+++ app/models/concerns/choosable.rb
@@ -6,7 +6,7 @@ module Choosable
     enumerize :choice, in: [:agree, :disagree, :unsure, :neutral], predicates: true, scope: true
     scope :agreed, -> { by_choice('agree') }
     scope :disagreed, -> { by_choice('disagree') }
-    scope :neutral, -> { by_choid('neutral') }
+    scope :neutral, -> { by_choice('neutral') }
     scope :unsure, -> { by_choice('unsure') }
     scope :sure, -> { where(choice: %w(agree disagree neutral)) }
     scope :by_choice, ->(choice) { where(choice: choice) }
