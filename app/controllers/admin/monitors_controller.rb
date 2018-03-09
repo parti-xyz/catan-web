90a64d6cc2be8f32c0c14af77161145cb68ea1b4
dali <dalikim@parti.xyz>
2018-03-10 01:32:08 +0900

    관리홈에서 빠띠 태그가 사라지고 페이지 로딩이 빨라집니다

--- app/controllers/admin/monitors_controller.rb
+++ app/controllers/admin/monitors_controller.rb
@@ -1,6 +1,5 @@
 class Admin::MonitorsController < Admin::BaseController
   def index
-    @issue_tags = ActsAsTaggableOn::Tag.where('taggings.taggable_type': 'Issue').joins(:taggings).select('name').distinct
     @statistics = Statistics.recent.limit(50)
   end
 end
