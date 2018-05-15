ae2ea35c57d0fc21a51fbdafd2357e633d2a1a1b
dali <dalikim@parti.xyz>
2018-05-15 20:13:48 +0900

    운영자가 활성빠띠를 조회할때 폐쇄된 빠띠는 제외합니다

--- app/controllers/admin/active_issue_stats_controller.rb
+++ app/controllers/admin/active_issue_stats_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ActiveIssueStatsController < Admin::BaseController
   def index
     @stat_at = params[:stat_at].try(:to_date) || Date.yesterday
-    @active_issue_stats = ActiveIssueStat.by_day(@stat_at, field: :stat_at).group_by { |i| i.issue.group_slug }
+    @active_issue_stats = ActiveIssueStat.by_day(@stat_at, field: :stat_at).reject{ |i| i.issue.blank? }.group_by { |i| i.issue.group_slug }
   end
 end
