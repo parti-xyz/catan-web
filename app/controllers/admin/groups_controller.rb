8e94de6f5aa4cdf5ea2c085e2ecf00b6d6ca6ccc
Greenhair <martionlee@gmail.com>
2018-05-18 10:16:56 +0900

    그룹을 삭제할 수 있습니다

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -2,4 +2,16 @@ class Admin::GroupsController < Admin::BaseController
   def index
     @groups = Group.where.not(slug: 'indie').sort_by_name.page(params[:page])
   end
+
+  def destroy
+    @group = Group.find_by(id: params[:id])
+    render_404 and return  if @group.blank?
+
+    if @group.destroy
+      flash[:success] = I18n.t('activerecord.successful.messages.deleted')
+    else
+      errors_to_flash @group
+    end
+    redirect_to admin_groups_path
+  end
 end
