6f55d945484f768fdd02746dc716c2cc608776a4
dali <dalikim@parti.xyz>
2018-07-25 20:35:00 +0900

    기술적인 수정 - 배포 후 테스트서버에서 사이드킥을 재시동합니다

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,10 @@
-on_utilities do
-  sudo "monit -g #{config.app}_sidekiq restart all"
+if 'production' == config.framework_env
+  on_utilities do
+    sudo "monit -g #{config.app}_sidekiq restart all"
+  end
+else
+  on_app_servers_and_utilities do
+    sudo "monit -g #{config.app}_sidekiq restart all"
+  end
 end
+
--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,7 +1,17 @@
-on_utilities do
-  worker_count = 1 # change as needed
-  (0...worker_count).each do |i|
-    sudo "monit stop all -g #{config.app}_sidekiq"
-    sudo "/engineyard/bin/sidekiq #{config.app} stop #{config.framework_env} #{i}"
+if 'production' == config.framework_env
+  on_utilities do
+    worker_count = 1 # change as needed
+      (0...worker_count).each do |i|
+        sudo "monit stop all -g #{config.app}_sidekiq"
+        sudo "/engineyard/bin/sidekiq #{config.app} stop #{config.framework_env} #{i}"
+      end
+  end
+else
+  on_app_servers_and_utilities do
+    worker_count = 1 # change as needed
+    (0...worker_count).each do |i|
+      sudo "monit stop all -g #{config.app}_sidekiq"
+      sudo "/engineyard/bin/sidekiq #{config.app} stop #{config.framework_env} #{i}"
+    end
   end
 end
