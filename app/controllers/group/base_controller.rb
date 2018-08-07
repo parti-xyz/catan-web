7bd6a540343350420f3912ef0b8cbf8bf8df1766
dali <dalikim@parti.xyz>
2018-08-08 07:15:31 +0900

    그룹 관리 페이지에서 권한이 없는 사람에게 적절한 메시지가 보입니다

--- app/controllers/group/base_controller.rb
+++ app/controllers/group/base_controller.rb
@@ -2,7 +2,10 @@ class Group::BaseController < ApplicationController
   before_action :verify_current_group
 
   def only_organizer
-    render_404 and return if !current_group.organized_by?(current_user) and !current_user.try(:admin?)
+    if !current_group.organized_by?(current_user) and !current_user.try(:admin?)
+      flash[:notice] = t('unauthorized.default')
+      redirect_to root_url and return
+    end
   end
 
   private
