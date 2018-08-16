873726a483b836074bfde3976636cf208114a22d
dali <dalikim@parti.xyz>
2018-08-17 01:35:18 +0900

    기술적인 수정 - 배포 중에 초기데이터가 세팅되지 않는 버그를 바로 잡습니다

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,6 +1,6 @@
 on_app_master do
   $stderr.puts "Seeding the data"
   run "cd #{config.release_path}"
-  run "bundle exec rake db:seed_fu"
-  run "bundle exec rake data:seed:group"
+  run "source #{config.shared_path}/config/env.custom && bundle exec rake db:seed_fu"
+  run "source #{config.shared_path}/config/env.custom && bundle exec rake data:seed:group"
 end
