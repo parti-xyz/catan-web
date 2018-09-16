667ecb09b2c3035b1c28a0f8dc4826a9d35d521d
dali <dalikim@parti.xyz>
2018-09-16 13:26:23 +0900

    기술적인 수정 - rails를 로딩할 때 환경변수를 함께 로딩합니다

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,9 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+require 'dotenv'
+env_file_path = "config/env.custom"
+if File.exists?(env_file_path)
+  Dotenv.load(env_file_path)
+end
