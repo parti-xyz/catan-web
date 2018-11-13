dcb89d7e0e01503dde8438bda8b7a37846562a6c
dalikim <dalikim@parti.xyz>
2018-11-13 16:06:19 +0900

    기술적인 수정 - Rack CORS Middleware를 asset하위까지 설정합니다

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module CatanWeb
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '/assets/*', :headers => :any, :methods => :get
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end
