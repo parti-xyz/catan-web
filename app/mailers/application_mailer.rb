9ed3a479adc11eff9c36d5ac31bcff12b08b2d1a
dalikim <dalikim@parti.xyz>
2019-04-19 07:28:33 +0900

    발송 메일 주소를 help@parti.xyz로 통일합니다

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,12 @@ class ApplicationMailer < ActionMailer::Base
   helper :application
   helper :parti_url
 
-  default from: "feed@parti.xyz"
+  default from: "빠띠 <help@parti.xyz>"
   layout 'email'
 
   private
 
   def build_from(user)
-    "#{"#{user.nickname} - 빠띠" if user.present?}<feed@parti.xyz>"
+    "#{"#{user.nickname} - 빠띠" if user.present?}<help@parti.xyz>"
   end
 end
