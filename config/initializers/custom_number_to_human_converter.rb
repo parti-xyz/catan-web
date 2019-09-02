3381afc0797de48218a1b9959e67c354201da34b
dalikim <dalikim@parti.xyz>
2019-09-02 12:54:11 +0900

    내 게시글 및 댓글 수를 사람이 읽는 것처럼 표기합니다

---
+++ config/initializers/custom_number_to_human_converter.rb
@@ -0,0 +1,5 @@
+ActiveSupport::NumberHelper::NumberToHumanConverter::DECIMAL_UNITS[8] = :hundred_million # 억
+ActiveSupport::NumberHelper::NumberToHumanConverter::DECIMAL_UNITS[4] = :ten_thousand  # 만
+ActiveSupport::NumberHelper::NumberToHumanConverter::INVERTED_DECIMAL_UNITS[:hundred_million] = 8
+ActiveSupport::NumberHelper::NumberToHumanConverter::INVERTED_DECIMAL_UNITS[:ten_thousand] = 4
+
