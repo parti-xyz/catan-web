8b67f8662ba0523fbf93a413ffef47ba74346597
dalikim <dalikim@parti.xyz>
2019-12-01 18:08:42 +0900

    기술적인 수정 - db migration할 때 기본 옵션 값 설정을 이모지 가능한 옵션으로 변경할 수 있게 합니다

--- config/initializers/mysql2_row_format_dynamic.rb
+++ config/initializers/mysql2_row_format_dynamic.rb
@@ -2,7 +2,7 @@ unless Rails.env.test?
   ActiveSupport.on_load :active_record do
     module AbstractMysqlAdapterWithInnodbRowFormat
       def create_table(table_name, options = {})
-        if options[:options].present?
+        if !options[:options].blank? and options[:options] != "ENGINE=InnoDB"
           abort "options을 지정할 수 없습니다. 마이그레이션을 중단합니다."
         end
         table_options = options.merge(:options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC')
