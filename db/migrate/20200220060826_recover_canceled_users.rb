21a69b583e7de0a47dbb3eab3e5e6b1fcd629633
dalikim <dalikim@parti.xyz>
2020-02-21 08:13:21 +0900

    버그 수정 - 탈퇴 회원 uid  마이그레이션 오류를 수정합니다 #438

--- db/migrate/20200220060826_recover_canceled_users.rb
+++ db/migrate/20200220060826_recover_canceled_users.rb
@@ -1,7 +1,9 @@
 class RecoverCanceledUsers < ActiveRecord::Migration[5.2]
   def change
     transaction do
-      execute "UPDATE users SET uid = '_____CANCEL_____#{SecureRandom.hex(10)}' WHERE uid is null"
+      User.all.each do |user|
+        execute "UPDATE users SET uid = '_____CANCEL_____#{SecureRandom.hex(10)}' WHERE uid is null and users.id = '#{user.id}'"
+      end
     end
   end
 end
