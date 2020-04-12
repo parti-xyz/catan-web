1ca48359214701f6d6d33c7d000b9b7417d7cbdb
dalikim <dalikim@parti.xyz>
2020-04-13 08:49:00 +0900

    기술적인 수정 - dart scss버전을 도입합니다

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,4 +10,13 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default'],
 }));
 
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function (element) {
+  return element.loader == 'sass-loader'
+})
+
+// Use Dart-implementation of Sass (default is node-sass)
+const options = sassLoaderConfig.options
+options.implementation = require('sass')
+
 module.exports = environment
