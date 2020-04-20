7b48fe4b3797119727ffcd46c5a6d92ccdf8e5bf
dalikim <dalikim@parti.xyz>
2020-04-20 20:08:46 +0900

    기술적인 수정 - 테스트 환경에서 배포 설정합니다

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -15,3 +15,10 @@ else
     end
   end
 end
+
+
+# create the shared packs folder. Should be a one-time only task
+run "mkdir -p #{config.shared_path}/packs"
+# link the shared packs folder to the release being deployed
+# so that packs are found even if assets haven't changed
+run "ln -nfs #{config.shared_path}/packs #{config.release_path}/public/packs"
