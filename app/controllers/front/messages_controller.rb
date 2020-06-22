7d38f81ae199acc5eba5ce5cace28e9dfc9bf205
dalikim <dalikim@parti.xyz>
2020-06-22 23:22:32 +0900

    모바일에서 그룹 이름 길이가 길 때를 대비합니다

--- app/controllers/front/messages_controller.rb
+++ app/controllers/front/messages_controller.rb
@@ -2,7 +2,7 @@ class Front::MessagesController < Front::BaseController
   def nav
     render_404 unless user_signed_in?
 
-    @messages = current_user.messages.of_group(current_group).recent.limit(30).to_a
+    @messages = current_user.messages.of_group(current_group).includes(:user, :sender).recent.limit(30).to_a
 
     @important_messages_count = current_user.important_messages_count(current_group)
 
