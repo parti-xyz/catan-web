f19b56437aa7eb4c2b6aca6397248f6874eb7030
dalikim <dalikim@parti.xyz>
2020-07-03 16:48:41 +0900

    그룹 생성할 때 기본 메뉴 색을 다듬습니다

--- db/migrate/20200703074650_change_default_navbar_bg_color_of_groups.rb
+++ db/migrate/20200703074650_change_default_navbar_bg_color_of_groups.rb
@@ -1,14 +1,14 @@
 class ChangeDefaultNavbarBgColorOfGroups < ActiveRecord::Migration[5.2]
   def change
-    change_column_default :groups, :navbar_coc_text_color, '#5e2abb'
+    change_column_default :groups, :navbar_bg_color, '#5e2abb'
 
     reversible do |dir|
       dir.up do
         transaction do
           execute <<-SQL
            UPDATE groups
-              SET navbar_coc_text_color = '#5e2abb'
-            WHERE navbar_coc_text_color = '5e2abb'
+              SET navbar_bg_color = '#5e2abb'
+            WHERE navbar_bg_color = '5e2abb'
           SQL
         end
       end
