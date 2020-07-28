ae15750e3a75dbc02393f2126729799395ff26ea
dalikim <dalikim@parti.xyz>
2020-07-28 14:00:38 +0900

    게시물 제목 스타일 지정 방식을 토글 버튼으로 변경합니다

--- app/javascript/compoments/editor/menus.js
+++ app/javascript/compoments/editor/menus.js
@@ -2,9 +2,9 @@ import { NodeSelection } from "prosemirror-state"
 import { wrapInList, liftListItem as liftListItemInList, sinkListItem as sinkListItemInList } from "prosemirror-schema-list"
 import { wrapItem, blockTypeItem, MenuItem, Dropdown, icons } from 'prosemirror-menu'
 import { undo, redo } from 'prosemirror-history'
-import { findSelectedNodeOfType } from 'prosemirror-utils'
+import { findParentNode } from 'prosemirror-utils'
 
-import { toggleMark, lift, joinUp } from 'prosemirror-commands'
+import { toggleMark, lift, joinUp, setBlockType } from 'prosemirror-commands'
 
 import { Prompt, TextField, ImageFileField } from './prompt'
 import { destroyMark, saveMark, toggleWrap } from './commands'
@@ -94,16 +94,16 @@ const buildMenuItems = (schema, uploadUrl, ruleFileSize) => {
     })
   }
   if (type = schema.nodes.paragraph) {
-    r.makeParagraph = blockTypeItem(type, {
+    r.makeParagraph = toggleBlockType(type, schema.nodes.paragraph, {
       title: "일반 형식",
-      label: "일반 형식"
+      icon: iconByClassName("fa fa-paragraph"),
     })
   }
   if (type = schema.nodes.heading) {
-    for (let i = 1; i <= 5; i++) {
-      r["makeHead" + i] = blockTypeItem(type, {
+    for (let i = 1; i <= 3; i++) {
+      r["makeHead" + i] = toggleBlockType(type, schema.nodes.paragraph, {
         title: '제목' + i,
-        label: '제목' + i,
+        icon: iconHeadingByClassName(i),
         attrs: { level: i }
       })
     }
@@ -137,12 +137,7 @@ const buildMenuItems = (schema, uploadUrl, ruleFileSize) => {
     return arr.filter(function (x) { return x })
   }
 
-  r.typeMenu = new Dropdown(cut([r.makeParagraph, r.makeHead1, r.makeHead2, r.makeHead3, r.makeHead4, r.makeHead5, r.makeHead6
-  ]), {
-    label: "스타일",
-  })
-
-  return [[r.typeMenu, r.toggleStrong, r.toggleEm, r.toggleStrike, r.toggleUnderline], [r.wrapBulletList, r.wrapOrderedList, r.liftList, r.sinkList, r.joinUp], [r.wrapBlockQuote, r.link, r.insertImage, r.insertHorizontalRule], [r.undo, r.redo]]
+  return [[r.makeParagraph, r.makeHead1, r.makeHead2, r.makeHead3   ], [r.toggleStrong, r.toggleEm, r.toggleStrike, r.toggleUnderline], [r.wrapBulletList, r.wrapOrderedList, r.liftList, r.sinkList, r.joinUp], [r.wrapBlockQuote, r.link, r.insertImage, r.insertHorizontalRule], [r.undo, r.redo]]
 }
 
 function cmdItem(cmd, options) {
@@ -200,6 +195,7 @@ function fromNodeInfo(selection, relativeHeight) {
     pos: (depth === 0 ? 0 : $from.before(depth)),
   }
 }
+
 function toggleList(listType, schema, attrs) {
   return function(state, dispatch) {
     if (!dispatch) { return true }
@@ -349,10 +345,60 @@ function toggleItem(nodeType, options) {
   return new MenuItem(passedOptions)
 }
 
+function toggleBlockType(nodeType, toggleType, options) {
+  const passedOptions = Object.assign({}, {
+    run: (state, dispatch) => {
+      const isActive = nodeIsActive(state, nodeType, options.attrs)
+
+      if (isActive) {
+        return setBlockType(toggleType)(state, dispatch)
+      }
+
+      return setBlockType(nodeType, options.attrs)(state, dispatch)
+    },
+    active: (state) => {
+      return nodeIsActive(state, nodeType, options.attrs)
+    },
+    enable: () => { return true },
+  }, options)
+  return new MenuItem(passedOptions)
+}
+
+function nodeIsActive(state, type, attrs = {}) {
+  const predicate = node => node.type === type
+  const parent = findParentNode(predicate)(state.selection)
+
+  if (!Object.keys(attrs).length || !parent) {
+    return !!parent
+  }
+
+  return parent.node.hasMarkup(type, attrs)
+}
+
 function iconByClassName(className) {
   let span = document.createElement("i")
   span.className = className
   return { dom: span }
+}
+
+function iconHeadingByClassName(i) {
+  const icons = {
+    h1: `<svg width = "1em" height = "1em" viewBox = "0 0 16 16" class="bi bi-type-h1" fill = "currentColor" xmlns = "http://www.w3.org/2000/svg" style="vertical-align: text-bottom;" >
+      <path d="M8.637 13V3.669H7.379V7.62H2.758V3.67H1.5V13h1.258V8.728h4.62V13h1.259zm5.329 0V3.669h-1.244L10.5 5.316v1.265l2.16-1.565h.062V13h1.244z" />
+    </svg >`,
+    h2: `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-type-h2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="vertical-align: text-bottom;" >
+      <path d="M7.638 13V3.669H6.38V7.62H1.759V3.67H.5V13h1.258V8.728h4.62V13h1.259zm3.022-6.733v-.048c0-.889.63-1.668 1.716-1.668.957 0 1.675.608 1.675 1.572 0 .855-.554 1.504-1.067 2.085l-3.513 3.999V13H15.5v-1.094h-4.245v-.075l2.481-2.844c.875-.998 1.586-1.784 1.586-2.953 0-1.463-1.155-2.556-2.919-2.556-1.941 0-2.966 1.326-2.966 2.74v.049h1.223z"/>
+    </svg>`,
+    h3: `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-type-h3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="vertical-align: text-bottom;" >
+      <path d="M7.637 13V3.669H6.379V7.62H1.758V3.67H.5V13h1.258V8.728h4.62V13h1.259zm3.625-4.272h1.018c1.142 0 1.935.67 1.949 1.674.013 1.005-.78 1.737-2.01 1.73-1.08-.007-1.853-.588-1.935-1.32H9.108c.069 1.327 1.224 2.386 3.083 2.386 1.935 0 3.343-1.155 3.309-2.789-.027-1.51-1.251-2.16-2.037-2.249v-.068c.704-.123 1.764-.91 1.723-2.229-.035-1.353-1.176-2.4-2.954-2.385-1.873.006-2.857 1.162-2.898 2.358h1.196c.062-.69.711-1.299 1.696-1.299.998 0 1.695.622 1.695 1.525.007.922-.718 1.592-1.695 1.592h-.964v1.074z"/>
+    </svg>`
+  }
+
+  var temp = document.createElement('div')
+  temp.innerHTML = icons['h' + i]
+  console.log(temp)
+
+  return { dom: temp    .firstChild }
 }
 
 export { buildMenuItems }
