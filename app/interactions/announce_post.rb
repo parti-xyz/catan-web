848794a46a457ef435b128c062ca6d283c0f4257
dalikim <dalikim@parti.xyz>
2020-09-03 12:43:34 +0900

    fix 필독 업데이트할 때 오류를 수정합니다

--- app/interactions/announce_post.rb
+++ app/interactions/announce_post.rb
@@ -22,15 +22,15 @@ class AnnouncePost < ActiveInteraction::Base
     else
       audience_user_ids = Member.where(id: announcement.audiences.select(:member_id)).pluck(:user_id).to_a
 
-      result[:not_member_users] = requesting_direct_announced_users.reject{ |user| audience_user_ids.include?(user.id) }
+      result[:not_member_users] = requesting_direct_announced_users.reject { |user| audience_user_ids.include?(user.id) }
     end
     result
   end
 
   def newbie_audiences(announcement, requesting_direct_announced_users)
-    members = post.issue.group.members.joins("LEFT OUTER JOIN audiences ON audiences.announcement_id = #{announcement.id} AND audiences.member_id = members.id", )
-      .where('audiences.id IS NULL')
-      .where('members.user_id in (?)', requesting_direct_announced_users)
+    members = post.issue.group.members.joins("LEFT OUTER JOIN audiences ON audiences.announcement_id = #{announcement.id} AND audiences.member_id = members.id")
+      .where("audiences.id IS NULL")
+      .where("members.user_id in (?)", requesting_direct_announced_users)
 
     members.each do |member|
       announcement.audiences.create(member: member)
@@ -40,8 +40,8 @@ class AnnouncePost < ActiveInteraction::Base
   def retired_audiences(announcement, requesting_direct_announced_users)
     direct_audiences_ids = post.issue.group.members.where(user: requesting_direct_announced_users).select(:id)
     audiences = announcement.audiences
-    .where('audiences.member_id not in (?)', direct_audiences_ids)
-    .where('audiences.noticed_at IS NULL')
+      .where("audiences.member_id not in (?)", direct_audiences_ids)
+      .where("audiences.noticed_at IS NULL")
     audiences.destroy_all
   end
 
@@ -49,7 +49,7 @@ class AnnouncePost < ActiveInteraction::Base
     return if current_user.blank?
 
     current_group_member = current_user.smart_group_member(post.issue.group)
-    current_audience = announcement.audiences.where(member: current_group_member)
+    current_audience = announcement.audiences.find_by(member: current_group_member)
 
     if current_audience.present?
       current_audience.noticed_at = DateTime.now
