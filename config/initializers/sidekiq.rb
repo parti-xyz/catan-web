eb9a9b1352c887145ea7b8f011e40a675505a9b8
dalikim <dalikim@parti.xyz>
2020-09-03 10:12:56 +0900

    chore 사이드킥을 설정을 변경합니다

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,27 +1,28 @@
-if ENV['SIDEKIQ'] != 'true' && (Rails.env.development? || Rails.env.test?)
-  require 'sidekiq/testing'
+if ENV["SIDEKIQ"] != "true" && (Rails.env.development? || Rails.env.test?)
+  require "sidekiq/testing"
   Sidekiq::Testing.inline!
-  ENV['SIDEKIQ'] = 'false'
+  ENV["SIDEKIQ"] = "false"
 else
   if Rails.env.production?
     Sidekiq.configure_server do |config|
       config.redis = {
-        url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
+        url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}",
+        namespace: "catan_web:#{Rails.env}",
       }
     end
     Sidekiq.configure_client do |config|
       config.redis = {
-        url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
+        url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}",
+        namespace: "catan_web:#{Rails.env}",
       }
     end
   else
     Sidekiq.configure_server do |config|
-      config.redis = {namespace: "catan_web:#{Rails.env}"}
+      config.redis = { namespace: "catan_web:#{Rails.env}" }
     end
     Sidekiq.configure_client do |config|
-      config.redis = {namespace: "catan_web:#{Rails.env}"}
+      config.redis = { namespace: "catan_web:#{Rails.env}" }
     end
-
   end
 
   schedule_file = "config/schedule.yml"
@@ -30,5 +31,5 @@ else
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
   end
 
-  ENV['SIDEKIQ'] = 'true'
+  ENV["SIDEKIQ"] = "true"
 end
