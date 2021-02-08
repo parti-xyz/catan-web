74adf2d189bd815e328032c057f87a86f9ac99de
Kim youngmin <dalikim@users.noreply.github.com>
2021-02-09 08:46:16 +0900

    fix: 간헐적으로 채널의 게시물 목록을 열때 채널 알림 설정이 의도치 않게 설정되던 것을 바로 잡습니다 (#511)

--- app/models/message_configuration/group_observation.rb
+++ app/models/message_configuration/group_observation.rb
@@ -7,7 +7,9 @@ class MessageConfiguration::GroupObservation < ApplicationRecord
   validates :user, uniqueness: { scope: [ :group_id ] }
 
   def self.of(user, group)
-    find_or_initialize_by(user_id: user.id, group_id: group.id) if user.present? && group.present?
+    return if user.blank? || group.blank?
+
+    find_or_initialize_by(user_id: user.id, group_id: group.id)
   end
 
   def parent
