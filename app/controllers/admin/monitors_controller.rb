bd7de2559bf4e0909aab958f44010c59a7c8e062
Kim youngmin <dalikim@users.noreply.github.com>
2021-05-17 07:58:05 +0900

    feat: 관리자에서 월별 사용자 가입 통계를 볼 수 있습니다 (#543)

--- app/controllers/admin/monitors_controller.rb
+++ app/controllers/admin/monitors_controller.rb
@@ -1,5 +1,7 @@
 class Admin::MonitorsController < Admin::BaseController
   def index
     @statistics = Statistics.recent.limit(100)
+
+    @user_join_statistics = User.with_deleted.group_by_month(:created_at, last: 24, reverse: true, format: "%Y %m").count
   end
 end
