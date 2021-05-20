2031b63020288b15b3bb81f870b458b346218a97
Kim youngmin <dalikim@users.noreply.github.com>
2021-05-21 01:27:43 +0900

    feat: 모니터링을 개선합니다 (#546)

--- app/controllers/admin/monitors_controller.rb
+++ app/controllers/admin/monitors_controller.rb
@@ -2,6 +2,8 @@ class Admin::MonitorsController < Admin::BaseController
   def index
     @statistics = Statistics.recent.limit(100)
 
-    @user_join_statistics = User.with_deleted.group_by_month(:created_at, last: 24, reverse: true, format: "%Y %m").count
+    @user_join_month = User.with_deleted.group_by_month(:created_at, last: 24, reverse: true, format: "%Y-%m").count
+    @user_join_week = User.with_deleted.group_by_week(:created_at, last: 24, reverse: true).count
+    @group_week = Group.with_deleted.group_by_week(:created_at, last: 24, reverse: true).count
   end
 end
